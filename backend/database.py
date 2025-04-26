444d45fb3e10a2cd8b9a954577af95c9d72839a5
yasharya2901 <yasharya2901@gmail.com>
2025-04-26 08:46:12 +0000

    .env file added

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost/speaking_book"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
